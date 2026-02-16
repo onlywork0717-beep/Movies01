8840ddc17ee97ae0c08150b945b23a532c5403fa
onlywork0717-beep <onlywork0717@gmail.com>
2026-02-16 13:08:10 +0530

    fixing sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,7 @@
-import { connectDB } from "../lib/mongodb";
-import Vlog from "../lib/models/Vlog";
+import { MetadataRoute } from "next";
 
-export default async function sitemap() {
-  const baseUrl = "https://moviedubbed.vercel.app"; // change in production
-
-  await connectDB();
-  const vlogs = await Vlog.find({}, "slug updatedAt");
-
-  const vlogUrls = vlogs.map((vlog: any) => ({
-    url: `${baseUrl}/vlog/${vlog.slug}`,
-    lastModified: vlog.updatedAt || new Date(),
-  }));
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://moviedubbed.vercel.app";
 
   return [
     {
@@ -19,16 +10,12 @@ export default async function sitemap() {
     },
     {
       url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
     },
     {
       url: `${baseUrl}/terms-condition`,
-      lastModified: new Date(),
     },
     {
       url: `${baseUrl}/disclaimer`,
-      lastModified: new Date(),
     },
-    ...vlogUrls,
   ];
 }
