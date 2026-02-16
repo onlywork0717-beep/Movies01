31e96f0f65ee67d38bb628ddaff791895572100f
onlywork0717-beep <onlywork0717@gmail.com>
2026-02-16 12:58:37 +0530

    updating robot file

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api"],
     },
-    sitemap: "http://localhost:3000/sitemap.xml",
+    sitemap: "https://moviedubbed.vercel.app/sitemap.xml",
   };
 }
